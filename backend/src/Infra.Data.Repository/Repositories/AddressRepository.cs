264c095e16a64ab96db303e0d0b3742e45680151
Lucas <lucaskreuch77@gmail.com>
2023-03-11 13:01:45 -0300

    update

---
+++ backend/src/Infra.Data.Repository/Repositories/AddressRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Domain.Interfeces.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Data.Repository.Repositories
+{
+    public class AddressRepository : BaseRepository<Address>, IAddressRepository
+    {
+    }
+}
