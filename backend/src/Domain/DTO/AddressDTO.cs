ef91b73b547a95400466c89017d227341b6ba176
Lucas <lucaskreuch77@gmail.com>
2023-03-10 23:40:21 -0300

    classe Address criada

---
+++ backend/src/Domain/DTO/AddressDTO.cs
@@ -0,0 +1,48 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTO
+{
+    public class AddressDTO
+    {
+        public int id { get; set; }
+        public string street { get; set; }
+        public string number { get; set; }
+        public string district { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+
+
+        #region Methods
+        public Address MapToEntity()
+        {
+            return new Address()
+            {
+                Id = id,
+                Street = street,
+                Number = number,
+                District = district,
+                City = city,
+                State = state
+            };
+        }
+
+        public AddressDTO MapToDTO(Address address)
+        {
+            return new AddressDTO()
+            {
+                id = address.Id,
+                street = address.Street,
+                number = address.Number,
+                district = address.District,
+                city = address.City,
+                state = address.State
+            };
+        }
+        #endregion
+    }
+}
