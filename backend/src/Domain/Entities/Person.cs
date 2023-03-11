d7a2bb5391e2647b64a00baf6283fab0a1327eaa
Lucas <lucaskreuch77@gmail.com>
2023-03-11 15:47:09 -0300

    entity person criada

---
+++ backend/src/Domain/Entities/Person.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birth { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CPF { get; set; }
+        public PersonTypeEnum Type { get; set; } 
+
+        public virtual IEnumerable<Occurrence>? Occurrences { get; set; }
+    }
+}
