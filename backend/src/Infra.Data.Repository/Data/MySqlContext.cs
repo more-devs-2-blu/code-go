7e46aab28ed704fdede2396465a90affd7fbf998
Lucas <lucaskreuch77@gmail.com>
2023-03-11 13:12:53 -0300

    mysql context

---
+++ backend/src/Infra.Data.Repository/Data/MySqlContext.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Data.Repository.Data
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+
+        #region DbSets
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Occurrence> Occurrences { get; set;}
+        //public DbSet<Person> People { get; set; }
+        #endregion
+    }
+}
