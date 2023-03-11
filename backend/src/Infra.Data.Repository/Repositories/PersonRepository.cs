7bbe99a74aa71f8d9e9dcddb516138794ccce3a7
Lucas <lucaskreuch77@gmail.com>
2023-03-11 15:55:52 -0300

    person repository adicionado

---
+++ backend/src/Infra.Data.Repository/Repositories/PersonRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Domain.Interfeces.IRepositories;
+using Infra.Data.Repository.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Data.Repository.Repositories
+{
+    public class PersonRepository : BaseRepository<Person>, IPersonRepository
+    {
+        public PersonRepository(MySqlContext context) : base(context)
+        {
+        }
+    }
+}
