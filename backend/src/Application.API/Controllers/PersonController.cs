a40edd54c9c2e39a43c94012268218d67fb23adf
Lucas <lucaskreuch77@gmail.com>
2023-03-12 10:17:47 -0300

    feat: adicionando person controller

--- backend/src/Application.API/Controllers/PersonController.cs
+++ backend/src/Application.API/Controllers/PersonController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Domain.DTO;
+using Domain.Interfeces.IServices;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,36 +10,59 @@ namespace Application.API.Controllers
     [ApiController]
     public class PersonController : ControllerBase
     {
+        private readonly IPersonService _personService;
+
+        public PersonController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
         // GET: api/<PersonController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult<IEnumerable<PersonDTO>>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return Ok(_personService.FindAll());
         }
 
         // GET api/<PersonController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<PersonDTO>> Get(int id)
         {
-            return "value";
+            PersonDTO person = new PersonDTO();
+
+            try
+            {
+                person = await _personService.FindById(id);
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+
+            return Ok(person);
         }
 
         // POST api/<PersonController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult<PersonDTO>> Post([FromBody] PersonDTO person)
         {
+            if (person == null) return BadRequest();
+            return new ObjectResult(await _personService.Save(person));
         }
 
         // PUT api/<PersonController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<ActionResult> Put(int id, [FromBody] PersonDTO person)
         {
+            if (person == null) return BadRequest();
+            return new ObjectResult(await _personService.Update(person));
         }
 
         // DELETE api/<PersonController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
+            return Ok(await _personService.Delete(id));
         }
     }
 }
