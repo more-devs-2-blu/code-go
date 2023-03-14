434f70d3b8ad736079789f3d86af82fc58fc96de
Jailson Roth <104799776+Jailsonr12@users.noreply.github.com>
2023-03-14 20:33:27 -0300

    aplicando o html e css do home e login

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const signUpButton: HTMLElement | null = document.getElementById('signUp');
+    const signInButton: HTMLElement | null = document.getElementById('signIn');
+    const container: HTMLElement | null = document.getElementById('container');
+
+    if (signUpButton && signInButton && container) {
+      signUpButton.addEventListener('click', (): void => {
+        container.classList.add("right-panel-active");
+      });
+
+      signInButton.addEventListener('click', (): void => {
+        container.classList.remove("right-panel-active");
+      });
+    }
+  }
 
 }
