1c8c367cc25df8db568a40e81904312cfd428bf8
Yasmin <yasminalvesdesouza27@gmail.com>
2023-03-15 10:57:18 -0300

    Feat: Formulário de Cadastro

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { PersonService } from './../../services/person.service';
+import { Login } from './../../models/login';
+import { Person } from './../../models/person';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,13 +10,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  formulario: any;
+  formularioLogin: any;
+  // @Input() register?: Person;
+  // @Input() login?: Login;
 
-  constructor() { }
+  // @Output() personsUpdated = new EventEmitter<Person[]>();
+
+  constructor(public service: PersonService) { }
 
   ngOnInit(): void {
+
+    this.formulario = new FormGroup({
+      name: new FormControl(null),
+      birth: new FormControl(null),
+      email: new FormControl(null),
+      password: new FormControl(null),
+      phone: new FormControl(null),
+      cpf: new FormControl(null),
+      type: new FormControl(null),
+      createdOn: new FormControl(null),
+    })
+
+    this.formularioLogin = new FormGroup({
+      email: new FormControl(null),
+      password: new FormControl(null),
+    })
+
+
     const signUpButton: HTMLElement | null = document.getElementById('signUp');
     const signInButton: HTMLElement | null = document.getElementById('signIn');
     const container: HTMLElement | null = document.getElementById('container');
+
+
 
     if (signUpButton && signInButton && container) {
       signUpButton.addEventListener('click', (): void => {
@@ -25,4 +55,19 @@ export class LoginComponent implements OnInit {
     }
   }
 
-}
+  enviarCadastro():void{
+    const person: Person = this.formulario.value;
+    this.service.postPerson(person).subscribe(resp=>{
+      alert('Pessoa inserida com sucesso');
+    })
+  }
+
+  // createPerson(person: Person){
+  //   this.service.postPerson(person).subscribe(
+  //       (resp: Person[]) =>
+  //         this.personsUpdated.emit(resp)
+
+  //   );
+  }
+
+
