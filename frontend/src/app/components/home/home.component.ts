6d004d39f0234b590f6c56393528104e4e7a17a8
Jailson Roth <104799776+Jailsonr12@users.noreply.github.com>
2023-03-15 18:40:24 -0300

    aqui ta funcionando

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,4 +1,3 @@
-
 import { ViewContainerRef } from '@angular/core';
 import { ViewChild } from '@angular/core';
 import { Component } from '@angular/core';
@@ -9,36 +8,8 @@ import { DynamicModalComponent } from '../dynamic-modal/dynamic-modal.component'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
-
-  
+  styleUrls: ['./home.component.scss'],
 })
-
-
 export class HomeComponent {
-  constructor(private modalService: ModalService) {}
-
-@ViewChild('modal', { read: ViewContainerRef, static: true })
-  entry!: ViewContainerRef;
-  sub!: Subscription;
-
-openModal() {
-// MyComponent é o componente que será renderizado dentro do seu body
-    this.sub = this.modalService
-      .openModal(this.entry, 'Título do modal', DynamicModalComponent)
-      .subscribe((v) => {
-        // dispara quando é aberto o modal
-      });
-  }
-
-   constructor(public service: PersonService){
-
-   }
-  ngOnInit(): void {
-    this.service.getAll().subscribe(
-      (resp) =>{
-        console.log(resp);
-      }
-    )
-  }
+  
 }
