c34b07f69945f2b2e045e9c7e27cf6fcb2cabe9d
Jailson Roth <104799776+Jailsonr12@users.noreply.github.com>
2023-03-16 15:22:24 -0300

    correção do login como pessoa

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -2,14 +2,21 @@ import { AuthService } from './../../services/auth.service';
 import { PersonService } from './../../services/person.service';
 import { Login } from './../../models/login';
 import { Person } from './../../models/person';
-import { Component, EventEmitter, Input, OnInit, Output, Type } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  Type,
+} from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   formulario: any;
@@ -19,10 +26,13 @@ export class LoginComponent implements OnInit {
 
   // @Output() personsUpdated = new EventEmitter<Person[]>();
 
-  constructor(public service: PersonService, public auth: AuthService, public router: Router) { }
+  constructor(
+    public service: PersonService,
+    public auth: AuthService,
+    public router: Router
+  ) {}
 
   ngOnInit(): void {
-
     this.formulario = new FormGroup({
       name: new FormControl(null),
       birth: new FormControl(null),
@@ -31,44 +41,41 @@ export class LoginComponent implements OnInit {
       cpf: new FormControl(null),
       // type: new FormControl(null),
       // createdOn: new FormControl(null),
-    })
+    });
 
     this.formularioLogin = new FormGroup({
       email: new FormControl(null),
       password: new FormControl(null),
-    })
-
+    });
 
     const signUpButton: HTMLElement | null = document.getElementById('signUp');
     const signInButton: HTMLElement | null = document.getElementById('signIn');
     const container: HTMLElement | null = document.getElementById('container');
 
-
-
     if (signUpButton && signInButton && container) {
       signUpButton.addEventListener('click', (): void => {
-        container.classList.add("right-panel-active");
+        container.classList.add('right-panel-active');
       });
 
       signInButton.addEventListener('click', (): void => {
-        container.classList.remove("right-panel-active");
+        container.classList.remove('right-panel-active');
       });
     }
   }
 
-  enviarCadastro():void{
+  enviarCadastro(): void {
     console.log(this.formulario);
     const person: Person = this.formulario.value;
-    this.service.postPerson(person).subscribe((resp)=>{
-      this.auth.login().then(()=>{
-        this.router.navigate(['/list-occurrence'])
-      })
-    })
+    person.type = 1;
+    localStorage.setItem("UserType", "1");
+    this.service.postPerson(person).subscribe((resp) => {
+      this.auth.login().then(() => {
+        this.router.navigate(['/list-occurrence']);
+      });
+    });
   }
 
-  enviarLogin():void{
-
-  }
+  enviarLogin(): void {}
 
   // createPerson(person: Person){
   //   this.service.postPerson(person).subscribe(
@@ -76,6 +83,4 @@ export class LoginComponent implements OnInit {
   //         this.personsUpdated.emit(resp)
 
   //   );
-  }
-
-
+}
