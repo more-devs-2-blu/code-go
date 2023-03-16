c184edbc407ab73e271109f1052975ba2416fd8f
Jailson Roth <104799776+Jailsonr12@users.noreply.github.com>
2023-03-16 06:38:09 -0300

    modal detalhes

--- frontend/src/app/components/list-occurrence/list-occurrence.component.ts
+++ frontend/src/app/components/list-occurrence/list-occurrence.component.ts
@@ -1,17 +1,17 @@
-// Yasmin
-// --------------
 import { Occurrence } from './../../models/occurrence';
 import { OccurrenceService } from '../../services/occurrence.service';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-occurrence',
   templateUrl: './list-occurrence.component.html',
   styleUrls: ['./list-occurrence.component.scss']
 })
-export class ListOccurrenceComponent implements OnInit{
+export class ListOccurrenceComponent implements OnInit {
+
+  public occurrenceSelected: Occurrence | undefined;
 
   public statusSelecionado: number | null = null;
   public categorySelecionado: number | null = null;
@@ -19,86 +19,70 @@ export class ListOccurrenceComponent implements OnInit{
 
   public login = localStorage.getItem("UserType")
 
-  public charactersList = new Observable<Occurrence[]>();
+  public occurrencesList$: Observable<Occurrence[]> | undefined;
 
   public listOccurrence: Occurrence[] = [];
 
-  constructor(public service: OccurrenceService){}
+  constructor(public service: OccurrenceService, private router: Router) { }
 
   ngOnInit(): void {
+    this.occurrencesList$ = this.service.getAll();
 
-    this.charactersList = this.service.getAll();
-
-    this.charactersList.subscribe(
-      (resp) =>{
-          this.listOccurrence = resp;
-          console.log(this.listOccurrence);
+    this.occurrencesList$.subscribe(
+      (resp) => {
+        this.listOccurrence = resp;
+        console.log(this.listOccurrence);
       }
     );
   }
 
   GetByStatus(i: any): void {
     console.log("Get by status", i)
-    this.charactersList = this.service.getByStatus(this.statusSelecionado);
+    this.occurrencesList$ = this.service.getByStatus(this.statusSelecionado);
 
-    this.charactersList.subscribe(
-      (resp) =>{
-          this.listOccurrence = resp;
-          console.log(this.listOccurrence);
+    this.occurrencesList$.subscribe(
+      (resp) => {
+        this.listOccurrence = resp;
+        console.log(this.listOccurrence);
       }
     );
   }
-
+ 
   GetByIdPerson(i: any): void {
+    this.occurrencesList$ = this.service.getByIdPerson({ i });
 
-    this.charactersList = this.service.getByIdPerson({ i });
-
-    this.charactersList.subscribe(
-      (resp) =>{
-          this.listOccurrence = resp;
-          console.log(this.listOccurrence);
+    this.occurrencesList$.subscribe(
+      (resp) => {
+        this.listOccurrence = resp;
+        console.log(this.listOccurrence);
       }
     );
   }
 
   GetByDistrict(i: any): void {
+    this.occurrencesList$ = this.service.getByDistrict(this.districtSelecionado);
 
-    this.charactersList = this.service.getByDistrict(this.districtSelecionado);
-
-    this.charactersList.subscribe(
-      (resp) =>{
-          this.listOccurrence = resp;
-          console.log(this.listOccurrence);
+    this.occurrencesList$.subscribe(
+      (resp) => {
+        this.listOccurrence = resp;
+        console.log(this.listOccurrence);
       }
     );
   }
 
   GetByCategory(i: any): void {
+    this.occurrencesList$ = this.service.getByCategory(this.categorySelecionado);
 
-    this.charactersList = this.service.getByCategory(this.categorySelecionado);
-
-    this.charactersList.subscribe(
-      (resp) =>{
-          this.listOccurrence = resp;
-          console.log(this.listOccurrence);
+    this.occurrencesList$.subscribe(
+      (resp) => {
+        this.listOccurrence = resp;
+        console.log(this.listOccurrence);
       }
     );
   }
 
+  selectOccurrence(occurrence: any) {
+    this.occurrenceSelected = occurrence;
+  }
+  
 }
-
-
-
-
-// Bernardo
-// --------------
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-list-occurrence',
-//   templateUrl: './list-occurrence.component.html',
-//   styleUrls: ['./list-occurrence.component.scss']
-// })
-// export class ListOccurrenceComponent {
-
-// }
